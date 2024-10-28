2e7f42c14873398c27da5901796c9d216a8c215e
yaswanthbandela <yaswanth@hurekatek.com>
2024-10-28 16:07:13 +0530

    remote state infra

---
+++ s3-dyanamodb.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "testing-terraform-state-bucket-2"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
